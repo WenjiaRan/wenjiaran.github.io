49cc24e1631d824128c966b443fceb529e096c56
WenjiaRan <399010199@qq.com>
2023-01-24 12:07:40 +0800

    Site updated: 2023-01-24 12:07:38

---
+++ js/my-own-fun-code/forbide-developer-mode.js
@@ -0,0 +1,127 @@
+(function(){
+		
+    document.onkeydown = function () {
+        if (window.event && window.event.keyCode == 123) {
+            event.keyCode = 0;
+            event.returnValue = false;
+            alert("不要扒我代码!");
+            return false;
+        }
+    var ctrlKey = e.ctrlKey || e.metaKey;
+    var shiftKey = e.shiftKey;
+    if(ctrlKey && shiftKey && e.keyCode == 73){
+        alert("不要扒我代码!");
+        e.keyCode=0;
+        e.returnValue=false;
+    }
+    };
+/**
+* 屏蔽右键菜单
+* @param event
+* @returns {boolean}
+*/
+document.oncontextmenu = function (event){
+if(window.event){
+    event = window.event;
+}try{
+    var the = event.srcElement;
+    if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+        alert("不准右键😜")
+        return false;
+    }
+    return true;
+}catch (e){
+    return false;
+}
+}
+
+/**
+* 屏蔽粘贴
+* @param event
+* @returns {boolean}
+*/
+document.onpaste = function (event){
+if(window.event){
+    event = window.event;
+}try{
+    var the = event.srcElement;
+    if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+        return false;
+    }
+    return true;
+}catch (e){
+    return false;
+}
+}
+
+/**
+* 屏蔽复制
+* @param event
+* @returns {boolean}
+*/
+document.oncopy = function (event){
+if(window.event){
+    event = window.event;
+}try{
+    var the = event.srcElement;
+    if(!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+        return false;
+    }
+    return true;
+}catch (e){
+    return false;
+}
+}
+
+/**
+* 屏蔽剪切
+* @param event
+* @returns {boolean}
+*/
+document.oncut = function (event){
+if(window.event){
+    event = window.event;
+}try{
+    var the = event.srcElement;
+    if(!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+        return false;
+    }
+    return true;
+}catch (e){
+    return false;
+}
+}
+
+/**
+* 屏蔽选中
+* @param event
+* @returns {boolean}
+*/
+document.onselectstart = function (event){
+if(window.event){
+    event = window.event;
+}try{
+    var the = event.srcElement;
+    if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+        return false;
+    }
+    return true;
+} catch (e) {
+    return false;
+}
+};
+
+/**
+* 尝试阻止开发者模式
+* @type {HTMLElement}
+*/
+var fuck = document.createElement('div');
+Object.defineProperty(fuck, 'id', {
+get:function(){
+    console.log("Hello World!");
+    location.href="about:blank";
+    history.replaceState();
+}
+});
+console.log(fuck);
+})();
