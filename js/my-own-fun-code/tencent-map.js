c8f568668f64b4231856b3e530003427c5bab857
WenjiaRan <399010199@qq.com>
2023-01-24 14:52:42 +0800

    Site updated: 2023-01-24 14:52:40

--- js/my-own-fun-code/tencent-map.js
+++ js/my-own-fun-code/tencent-map.js
@@ -34,7 +34,7 @@ function getDistance(e1, n1, e2, n2) {
 function showWelcome() {
     if (!document.getElementById("welcome-info")) return
 
-    let dist = getDistance(120.57186, 31.29579, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里记录你自己的经纬度
+    let dist = getDistance(105.8870,29.3624, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里记录你自己的经纬度
 
     let pos = ipLoacation.result.ad_info.nation;
     let posdesc;
